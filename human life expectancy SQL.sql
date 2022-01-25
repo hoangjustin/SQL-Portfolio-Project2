e4d3ee32bb1c758516abbb445a44fbd40acf126f
Justin Nguyen <67940255+hoangjustin@users.noreply.github.com>
2022-01-25 00:49:03 -0800

    Add files via upload

--- human life expectancy SQL.sql
+++ human life expectancy SQL.sql
@@ -159,18 +159,3 @@ where humanlifeCTE.rownumber = 1 or humanlifeCTE2.rownumber = 1
 order by humanlifeCTE.country
 
 
-
---View new table with year and avg column
-select country, 
-	    "1990" , "1991" , "1992" , "1993" , "1994" , 
-		"1995" , "1996" , "1997" , "1998" , "1999" , 
-		"2000" , "2001" , "2002" , "2003" , "2004" , 
-		"2005" , "2006" , "2007" , "2008" , "2009" ,
-		"2010" , "2011" , "2012" , "2013" , "2014" ,
-		"2015" , "2016" , "2017" , "2018" , "2019"
-from humanlife
-where country in ('Afghanistan', 'United States', 'United Kingdom', 'Vietnam', 'China', 'Canada', 'Austrailia', 'Mexico', 'Sweden', 'India') and
-	  level = 'National'
-	  
-select country, 
-
